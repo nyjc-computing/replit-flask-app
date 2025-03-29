bb113c20873ec5fcab327a4aac737331b467990d
JS Ng <ngjunsiang@gmail.com>
2025-03-30 00:44:35 +0800

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Create and activate a virtual environment
-python -m venv .venv
-source .venv/bin/activate
+# Configure Git to use fast-forward pulls
+git config pull.ff true
 
 # Install Poetry and project dependencies
 pip install poetry
 poetry install
 
-# Configure Git to use fast-forward pulls
-git config pull.ff true
+# Activate poetry venv (for pylance auto-import)
+npx vscode-tasks run poetry-activate-venv
